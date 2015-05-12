bfef46b0407cf15f6373b7fa9db08dbd27bd0132
Pedro Santos <donbonifacio@gmail.com>
2015-05-12 09:17:57 +0100

    Migrate source code

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject clanhr/ring-test-client "0.1.0"
+  :description "Utilities for testing controllers on top of ring"
+  :url "https://github.com/clanhr/ring-test-client"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+                 [clanhr/reply "0.2.0"]
+                 #_[ring/ring-core "1.3.2"]
+                 [ring/ring-mock "0.2.0"]])
